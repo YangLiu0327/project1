47a2d2190701cca3bd231e44bf5cc638126e6b84
liuyang <yangliuwork0327@qq.com>
2021-04-25 03:02:03 +1000

    yangliuwebsite

---
+++ page/main.js
@@ -0,0 +1,15 @@
+// downloas document
+function downfile() {
+    let url= "./jianli000.pdf";
+    window.location.href=url;
+}
+
+//页面防跳动
+// function showDiv() {
+//   $('.container').show();
+// }
+// setTimeout(showDiv, 100);
+
+
+
+
