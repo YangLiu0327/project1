878845a52738e2850293e6ff15375fde8bab19bb
liuyang <liuyangyangyang1992@gmail.com>
2022-04-13 22:49:52 +1000

    update formate

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -4,20 +4,18 @@ import {Link} from 'react-router-dom';
 import {Wrap, Brief, Photo, Title, List, ListItem, Button } from './style';
 
 
-
 class HomePage extends Component{
     render(){
         return <Wrap>
                  {/* introduce */}
                  <Brief>   
                      <Photo src={yangUrl} alt="yang-phtot"></Photo>
-                     < Title> Yang LIU </ Title>
-                     <List >
-                         <ListItem > Front-end Developer</ListItem >
-                         <ListItem > Full Stack Developer</ListItem >
-                         <ListItem > React </ListItem >
-                     </List >
-                     
+                     <Title> Yang LIU </ Title>
+                     <List>
+                         <ListItem> Front-end Developer</ListItem>
+                         <ListItem> Full Stack Developer</ListItem>
+                         <ListItem> React </ListItem >
+                     </List>
                     <Link to="/about">
                         <Button>Vcard style</Button>
                     </Link>
