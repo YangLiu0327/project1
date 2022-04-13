62db42a7e73cf04ed1f0431f85c56d3ad9fe1b81
liuyang <liuyangyangyang1992@gmail.com>
2022-04-13 20:07:41 +1000

    delete comments

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,13 +1,8 @@
 import React from "react"
 import { DropdownButton, DropdownBox } from "./style"
-import {Link} from 'react-router-dom';
 
 const Dropdown = () => (
   <DropdownBox>
-    {/* <DropdownButton as={Link} to="/">Home</DropdownButton>
-    <DropdownButton as={Link} to="/about">About</DropdownButton>
-    <DropdownButton as={Link} to="/resume">Resume</DropdownButton>
-    <DropdownButton as={Link} to="/contact">Contact</DropdownButton> */}
     <DropdownButton href="/">Home</DropdownButton>
     <DropdownButton href="/about">About</DropdownButton>
     <DropdownButton href="/resume">Resume</DropdownButton>
